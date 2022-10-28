15036d8a7649be9cd47cee9c48c2b7896a267fb5
alexeyShuncko <shuncko.a@yandex.ru>
2022-10-28 16:43:12 +0300

    Ссылки

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -1,15 +1,16 @@
 import Head from "next/head"
 import Link from "next/link"
-import { useRouter } from "next/router"
 import s from '../styles/Layout.module.css'
 
 
 export const MainLayout = ({ children }) => {
 
-    const router = useRouter()
-
-    const clickHandler =()=> {
-        router.push('/')
+    const clickHandler =(e)=> {
+        if (e.target.localName === 'a') {
+            let list =  e.currentTarget.querySelectorAll('a') 
+            list.forEach(el => el.classList.remove(s.active))
+            e.target.classList.toggle(s.active)
+        }  
     }
 
     return (
@@ -17,8 +18,8 @@ export const MainLayout = ({ children }) => {
             <Head>
                 <title>Shop</title>
             </Head>
-            <nav className={s.navigation}>
-                <span onClick={clickHandler}>Online store</span>
+            <nav className={s.navigation} onClick={clickHandler}>
+                <Link href={'/'} className={s.active} >Главная</Link>
                 <Link href={'/basket'}>Корзина</Link>
             </nav>
             <main className={s.container}>
