420f47771f039b30d2a15ec0b2b2be77469906e3
alexeyShuncko <shuncko.a@yandex.ru>
2022-10-31 17:31:08 +0300

    Каталог товаров, fake api

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -3,24 +3,22 @@ import Link from "next/link"
 import s from '../styles/Layout.module.css'
 
 
+
+
+
 export const MainLayout = ({ children }) => {
 
-    const clickHandler =(e)=> {
-        if (e.target.localName === 'a') {
-            let list =  e.currentTarget.querySelectorAll('a') 
-            list.forEach(el => el.classList.remove(s.active))
-            e.target.classList.toggle(s.active)
-        }  
-    }
+
+    let count = 5
 
     return (
         <>
             <Head>
                 <title>Shop</title>
             </Head>
-            <nav className={s.navigation} onClick={clickHandler}>
-                <Link href={'/'} className={s.active} >Главная</Link>
-                <Link href={'/basket'}>Корзина</Link>
+            <nav className={s.navigation} >
+                <Link href={'/'} >Каталог</Link>
+                <Link href={'/basket'} className={s.basket} data-count={count} >Корзина</Link>
             </nav>
             <main className={s.container}>
                 {children}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
 import { MainLayout } from '../components/MainLayout'
+import Image from 'next/image'
+import s from '../styles/Catalog.module.css'
 
-export default function Home() {
+export default function Catalog({data}) {
+
   return (
     <MainLayout>
-     Online store
+      <div>
+      Online store
+      </div>
+      <div className={s.catalog}>
+      {data.length !== 0 &&
+        data.map(el => (
+          <div key={el.title} className={s.card}>
+            <div>{el.title}</div>
+            <div><Image src={`${el.image}`} alt='' width={100} height={130} priority/></div>
+            <div className={s.price}>{el.price}$</div>
+            <div>
+            <button className={s.btn}>В корзину</button>
+            <button className={s.btn}>Подробнее</button>
+            </div>
+          </div>
+        ))
+      }
+      </div>
     </MainLayout>
   )
+}
+
+
+Catalog.getInitialProps = async () => {
+  const response = await fetch('https://fakestoreapi.com/products')
+  const data = await response.json()
+  return {
+    data
+  }
 }
