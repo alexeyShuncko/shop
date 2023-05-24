ae4e428dd2b148677f0f1a42b880d097b9bf5bdf
alexeyShuncko <shuncko.a@yandex.ru>
2023-05-24 15:19:47 +0300

    Кнопка оплаты

--- pages/payment.js
+++ pages/payment.js
@@ -1,3 +1,19 @@
-export default function Payment() {
-  return <div>Оплата</div>;
+import { useRouter } from 'next/router';
+
+export default function Payment({ setVisibl, setText, setBasket }) {
+  const router = useRouter();
+  const handlerPayment = () => {
+    router.push(`/`);
+    setBasket([]);
+    setText('Успешно оплачено!');
+    setVisibl(true);
+  };
+  return (
+    <div>
+      <div>Оплата</div>
+      <button className="btn success" onClick={handlerPayment}>
+        Оплатить
+      </button>
+    </div>
+  );
 }
