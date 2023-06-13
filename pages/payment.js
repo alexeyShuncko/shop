4478ae32ad8ea8acb61196bcda5818d2fd1c4d80
alexeyShuncko <shuncko.a@yandex.ru>
2023-06-13 14:41:12 +0300

    required input

--- pages/payment.js
+++ pages/payment.js
@@ -3,9 +3,15 @@ import Image from 'next/image';
 import s from '../styles/Payment.module.css';
 import chip from '../public/chip.png';
 import visa from '../public/visa.png';
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 
-export default function Payment({ setVisibl, setText, setBasket }) {
+export default function Payment({
+  setVisibl,
+  setText,
+  setBasket,
+  basket,
+  currency,
+}) {
   const router = useRouter();
   const [data, setData] = useState({
     num1: '',
@@ -20,13 +26,26 @@ export default function Payment({ setVisibl, setText, setBasket }) {
 
   const handlerPayment = (e) => {
     e.preventDefault();
+    for (const key in data) {
+      if (Object.hasOwnProperty.call(data, key)) {
+        const element = data[key];
+        if (element.length === 0) {
+          const inp = document.getElementById(key);
+          inp.classList.add(s.error);
+          inp.focus();
+          return;
+        }
+      }
+    }
     router.push(`/`);
-    setBasket([]);
     setText('Успешно оплачено!');
     setVisibl(true);
+    setBasket([]);
   };
 
   const jump = (e) => {
+    e.target.classList.contains(s.error) && e.target.classList.remove(s.error);
+
     const regex1 = /[^0-9]/;
     const regexEng = /[^A-Za-z ]/;
 
@@ -54,14 +73,30 @@ export default function Payment({ setVisibl, setText, setBasket }) {
 
   return (
     <div className={s.payment}>
-      <div>Оплата</div>
+      <div>
+        {currency === 'BYN' ? (
+          <span className={s.textTotal}>
+            К оплате:{' '}
+            {basket
+              .map((el) => el.price * el.amount * 2.5)
+              .reduce((acc, sum) => acc + sum, 0)
+              .toFixed(2)}{' '}
+            Br
+          </span>
+        ) : (
+          <span className={s.textTotal}>
+            К оплате:{' '}
+            {basket
+              .map((el) => el.price * el.amount)
+              .reduce((acc, sum) => acc + sum, 0)
+              .toFixed(2)}{' '}
+            $
+          </span>
+        )}
+      </div>
       <div className={s.cardBlock}>
         <div className={s.card1}>
           <div className={s.strip}></div>
-          <div className={s.cvc}>
-            <input maxLength={3} id="CVC" onChange={jump} value={data.CVC} />
-            <span>CVC</span>
-          </div>
         </div>
         <div className={s.card}>
           <div className={s.bank}>
@@ -132,12 +167,24 @@ export default function Payment({ setVisibl, setText, setBasket }) {
               />
               <span>Имя владельца</span>
             </div>
-            <button
-              type={'submit'}
-              className="btn success"
-              onClick={handlerPayment}>
-              Оплатить
-            </button>
+            <div className={s.cvc}>
+              <input maxLength={3} id="CVC" onChange={jump} value={data.CVC} />
+              <span>CVC</span>
+            </div>
+            <div className={s.btnBlock}>
+              <button
+                type={'button'}
+                className="btn normal"
+                onClick={() => router.push(`/basket`)}>
+                В корзину
+              </button>
+              <button
+                type={'submit'}
+                className="btn success"
+                onClick={handlerPayment}>
+                Оплатить
+              </button>
+            </div>
           </form>
         </div>
       </div>
